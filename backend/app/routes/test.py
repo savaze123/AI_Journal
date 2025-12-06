0807fa4bc3248c5a197bacf2e320ec15eb944bb7
savaze123 <60440545+savaze123@users.noreply.github.com>
2025-12-05 19:34:05 -0500

    initial commit on my feature branch

--- backend/app/routes/test.py
+++ backend/app/routes/test.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, jsonify 
+from flask import Blueprint 
 
-test_bp = Blueprint("test",__name__)
+test_bp = Blueprint('test', __name__, url_prefix = "/api")
 
-@test_bp.route("/hello")
+@test_bp.get("/hello")
 def hello():
-    return jsonify({"message": "Hi from Flask Man"})
+  return {"message": "Hello from Flask"}
